114c921460ec983f83384d230abb7997de3e9bbd
Letícia Domingues <leticia.domingues@opus-software.com.br>
2020-10-17 15:49:09 -0300

    Dinamically adding cards

--- public/main.js
+++ public/main.js
@@ -1,8 +1,40 @@
 
-var box = document.querySelector('.box');
-var button = document.querySelector('button');
+const placesArea = document.getElementById("places-area");
+const addPlaceButton = document.getElementById("add-place-button");
 
 var bannerToInstallApp;
+var places = [
+  {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  },
+  {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  },
+  {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  },
+  {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  },
+  {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  },
+  {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  }
+];
 
 function installServiceWorker() {
   if ('serviceWorker' in navigator) {
@@ -12,18 +44,53 @@ function installServiceWorker() {
   }
 }
 
-button.addEventListener('click', () => {
-  openBannerToInstallApp();
-  /*
-  if (box.classList.contains('visible')) {
-    box.classList.remove('visible');
-  } else {
-    box.classList.add('visible');
+function createCards() {
+  for (var place of places) {
+    addNewPlace(place);
   }
-  */
+}
+
+function addNewPlace(place) {
+  // Creating card section
+  var cardSection = document.createElement('section');
+  cardSection.className = 'mdl-card mdl-shadow--2dp card';
+
+  // Creating card title wrapper
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title title-wrapper';
+  cardTitle.style.backgroundImage = `url("${place.image}")`;
+  cardSection.appendChild(cardTitle);
+
+  // Creating card title text element
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = place.title;
+  cardTitle.appendChild(cardTitleTextElement);
+
+  // Creating card description
+  var cardDescription = document.createElement('div');
+  cardDescription.className = 'mdl-card__supporting-text';
+  cardDescription.textContent = place.description;
+  cardDescription.style.textAlign = 'center';
+  cardSection.appendChild(cardDescription);
+
+  // Adding new card to html
+  placesArea.appendChild(cardSection);
+}
+
+addPlaceButton.addEventListener('click', () => {
+  openBannerToInstallApp();
+  var newPlace = {
+    title: "Monterey Bay",
+    description: "Monterey Bay Aquarium was fun!",
+    image: "/src/images/monterey.jpg"
+  };
+  places.push(newPlace);
+  addNewPlace(newPlace);
 });
 
 installServiceWorker();
+createCards();
 
 function fetchExample() {
   fetch('https://httpbin.org/ip')
@@ -32,12 +99,8 @@ function fetchExample() {
   })
   .then(function(data) {
     console.log(data.origin);
-    //box.style.height = (data.origin.substr(0, 2) * 5) + 'px';
   });
 }
-
-
-
 
 function openBannerToInstallApp() {
   if (bannerToInstallApp) {
